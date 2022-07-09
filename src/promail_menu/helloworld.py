fa96db24ba7ef8cc1a09be248c1a2498094a5a4f
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2022-07-09 14:53:13 -0400

    Implement Hello World

---
+++ src/promail_menu/helloworld.py
@@ -0,0 +1,20 @@
+# src/promail_menu/helloworld.py
+"""Hello World Example for sending and recieving emails"""
+from promail.clients import GmailClient
+from promail_template.templates.full import HelloWorld
+
+# constants
+MENU_EMAIL = "promail.tests@gmail.com"
+AUTHORIZED_EMAILS = ("my_email@example.com", "someone_else@example.com")
+
+# set up client
+client = GmailClient(MENU_EMAIL, credentials="../.credentials/gmail_credentials.json")
+
+template = HelloWorld({"name":"Antoine"})
+
+client.send_email(
+    recipients="my_email@example.com",
+    subject="Hello from Promail",
+    htmltext=template.html,
+    plaintext=template.plaintext
+)
